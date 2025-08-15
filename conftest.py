84e1e2448f4d4213b19ef9e769aafb1cf6f8c8d6
Nikolay Shirokov <shirokiy93@gmail.com>
2025-08-16 00:28:27 +0700

    fixed format

--- conftest.py
+++ conftest.py
@@ -6,15 +6,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 # Фикстура: Faker (генератор фейковых данных)
 @pytest.fixture
 def faker():
     return Faker("ru_RU")
 
+
 # Фикстура: DRF API-клиент (будем отправлять запросы)
 @pytest.fixture
 def api_client():
     return APIClient()
+
 
 # Фикстура: фейковый пользователь
 @pytest.fixture
